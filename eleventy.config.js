bb53a23ba6245614c0f0352ef61b84adb0a4aadf
Johan N. Martin <jnm+johanmartindev@catenare.com>
2025-10-29 09:24:31 +0200

    Chore: configure eleventy for the site

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,10 @@
+import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
 export default async function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setLayoutsDirectory("_layouts");
   eleventyConfig.setOutputDirectory("dist");
+  eleventyConfig.setTemplateFormats("html,liquid,njk");
+  eleventyConfig.watchIgnores.add("README.md");
+  eleventyConfig.addPassthroughCopy("src/style.css");
+  eleventyConfig.addPlugin(EleventyVitePlugin);
 }
